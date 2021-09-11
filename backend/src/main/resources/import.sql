5ec31ad95d4f91472e5467e6b747d16b86af6a15
jajunio333 <jajunio333@gmail.com>
2021-09-11 00:01:31 -0300

    Group by search

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,8 +1,8 @@
-INSERT INTO tb_sellers(name) VALUES ('Logan');
-INSERT INTO tb_sellers(name) VALUES ('Anakin');
-INSERT INTO tb_sellers(name) VALUES ('BarryAllen');
-INSERT INTO tb_sellers(name) VALUES ('Kal-El');
-INSERT INTO tb_sellers(name) VALUES ('Padme');
+INSERT INTO tb_sellers(name) VALUES ('Jurandir');
+INSERT INTO tb_sellers(name) VALUES ('Nelio');
+INSERT INTO tb_sellers(name) VALUES ('Karol');
+INSERT INTO tb_sellers(name) VALUES ('Manuel');
+INSERT INTO tb_sellers(name) VALUES ('Jordan');
 
 INSERT INTO tb_sales(seller_id,visited,deals,amount,date) VALUES (5,83,66,5501.0,'2021-04-01');
 INSERT INTO tb_sales(seller_id,visited,deals,amount,date) VALUES (5,113,78,8290.0,'2021-03-31');
